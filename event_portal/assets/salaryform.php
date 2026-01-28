4311d2a4a01a9b78756ab69fa3a282536aa4eafe
MGCAZPER <tharindumarasinghe56@gmail.com>
2026-01-28 14:42:13 +0530

    Create salaryform.php

---
+++ event_portal/assets/salaryform.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Salary Form</h1>
+    <form action="salary-summary.php" method="POST">
+        <label> Employee Name: </label>
+        <input type="text" name="emp_name" id="emp_name"><br><br>
+        <label> Basic Salary:  </label>
+        <input type="text" name="Basic_sal" id="Basic_sal"><br><br>
+        <label> OT Hours: : </label>
+        <input type="text" name="oth" id="oth"><br><br>
+        <button type="text" name="submit" id="submit"> Submit </button>
+    </form>
+</body>
+</html>
