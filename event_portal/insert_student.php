f8b7d0f875f427168f6abe8779b5d29c82930243
MGCAZPER <tharindumarasinghe56@gmail.com>
2026-01-28 18:03:42 +0530

    Create insert_student.php

---
+++ event_portal/insert_student.php
@@ -0,0 +1,16 @@
+<?php
+    $name=$_POST['student_name'];
+    $city=$_POST['student_city'];
+    $course=$_POST['course'];
+
+    include_once 'dbcon.php';                       //import the code
+    $sql="INSERT INTO student (St_Name,city,course) VALUES ('$name','$city','$course')";
+    $result=mysqli_query($con,$sql);                //For DML Operation function return the number of records effected 
+    
+    if($result>0)
+        {
+            header('location:create_student.php'); //Redirect User to dashboard
+    }else{
+        header('location:students.php'); //Redirect User to form page
+    }
+?>
