bdc6f5bbc920e1e24f3492d1faee57a5062380c4
MGCAZPER <tharindumarasinghe56@gmail.com>
2026-01-28 11:49:56 +0530

    Update index.php

--- DMW Project/PHP/index.php
+++ DMW Project/PHP/index.php
@@ -1,3 +1,6 @@
+<?php
+    $titles=["Mr","Mrs","Rev","Dr"];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +9,16 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello world</h1>
+    <h1> Titles list</h1>
+    <label> title: </label>
+    <select name="title" id="title">
+        <option value="">Select a title</option>
+        <?php for($i=0;$i<sizeof($titles);$i++){ ?>
+            <option value="<?php echo $titles[$i]; ?>"> 
+                <?php echo $titles[$i]; ?> 
+            </option>
+        <?php } ?>
+    </select>   
+
 </body>
 </html>
